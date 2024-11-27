0902aa4c7c3fe0f551a98c547afde818b0f068a2
pallavi-2 <pallavipandey299@gmail.com>
2024-11-27 22:34:05 +0545

    Updated EmailSender interface

--- Interfaces/IEmailSender.cs
+++ Interfaces/IEmailSender.cs
@@ -2,6 +2,6 @@
 {
     public interface IEmailSender
     {
-        void SendEmail(string toEmail, string subject);
+        void SendEmail(string toEmail, string subject, string content);
     }
 }
